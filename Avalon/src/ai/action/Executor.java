6d7ab88be3ffee476e6534a4e33a4ea50f03abd7
detentor <vinicius.seufitele@gmail.com>
2015-07-27 22:04:23 -0300

    Criação de novas classes

---
+++ Avalon/src/ai/action/Executor.java
@@ -0,0 +1,14 @@
+package ai.action;
+
+import ai.model.Match;
+
+public class Executor
+{
+	public static void main(String[] args)
+	{
+		//Cria uma partida
+		Match match = new Match();
+		
+	}
+
+}
